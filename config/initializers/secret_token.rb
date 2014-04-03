7dbf9351aafe0dc9b94c7d608d952a5d2843e9ba
Robel <robeltadesse01@gmail.com>
2014-04-03 19:03:02 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ethioboston::Application.config.secret_key_base = 'a3aea6c8fb32b0acbe0c22221b140e046a5bd0f341c41c3d1f0af2d6c61ebcf42dc7a99a0f73ec53c52cedfa29eb3eec8cde0b649d3a43f970f59b62c4e7911f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ethioboston::Application.config.session_store :cookie_store, key: '_ethioboston_session'
