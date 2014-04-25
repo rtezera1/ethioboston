3b4305985b09ac3427b0eebba35ebb56050c772b
Robel <robeltadesse01@gmail.com>
2014-04-25 11:52:59 -0400

    fix bug email

--- app/workers/housing_notifier_worker.rb
+++ app/workers/housing_notifier_worker.rb
@@ -5,13 +5,13 @@ class HousingNotifierWorker
   def perform(housing_id)
     CallList.find_each do |user|
       if user.reason == 'Homes' || user.reason == 'Both'        
-        housing = Housing.find(housing_id)
+        @housing = Housing.find(housing_id)
         message = Message.new
         message.send_text( user.phone_number, housing.type_of_housing )
       end
     end
 
-      Notification.new_house.deliver
+      Notification.new_house(@housing).deliver
   end
 end
 
--- app/workers/jobs_notifier_worker.rb
+++ app/workers/jobs_notifier_worker.rb
@@ -5,11 +5,13 @@ class JobsNotifierWorker
   def perform(job_id)
     CallList.find_each do |user|
       if user.reason == 'Jobs' || user.reason == 'Both'
-        job = Job.find(job_id)
+        @job = Job.find(job_id)
         message = Message.new
         message.send_text( user.phone_number, job.job_title )
       end
     end
-      Notification.new_job.deliver
+
+      Notification.new_job(@job).deliver
+
   end
 end
