483504b39297854e618f8e177be125bd80b93e9e
Robel <robeltadesse01@gmail.com>
2014-04-25 16:20:35 -0400

    fixed unicorn

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: bundle exec unicorn -p $PORT -c ./config/unicorn.rb
+web: bundle exec unicorn -p $PORT -E $RACK_ENV -c ./config/unicorn.rb
 worker: bundle exec sidekiq -e $RACK_ENV -C config/sidekiq.yml
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -17,6 +17,15 @@ after_fork do |server, worker|
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
 
+  Sidekiq.configure_client do |config|
+    config.redis = { :size => 1 }
+  end
+  Sidekiq.configure_server do |config|
+    config.redis = { :size => 5 }
+  end
+
   defined?(ActiveRecord::Base) and
     ActiveRecord::Base.establish_connection
+
+
 end
